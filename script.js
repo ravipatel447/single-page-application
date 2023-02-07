0c12fc73a6bb89d253bc2ef8149bb8c304f3a5ad
ravipatelsimform <ravi.patel@simformsolutions.com>
2023-02-07 17:52:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const section1 = document.querySelector("#section--1");
+const btnToScrool = document.querySelector(".btn--scroll-to");
+const navLinks = document.querySelector(".nav__links");
+const nav = document.querySelector(".nav");
+
+////////////////////////////scrool effect/////////////////////////////
+btnToScrool.addEventListener("click", () => {
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+navLinks.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+//////////////////////////Image slider///////////////////////////
+const slide = function () {
+  const slidLeft = document.querySelector(".slider__btn--left");
+  const slidRight = document.querySelector(".slider__btn--right");
+  const slide = document.querySelectorAll(".slide");
+  const total = slide.length;
+  let current = 0;
+  slidRight.addEventListener("click", () => {
+    current = (current + 1) % total;
+    slide.forEach((e, index) => {
+      e.style.transform = `translateX(${(index - current) * 100}%)`;
+    });
+  });
+  slidLeft.addEventListener("click", () => {
+    if (current < 1) {
+      current = (total - current - 1) % total;
+    } else {
+      current = (current - 1) % total;
+    }
+    slide.forEach((e, index) => {
+      e.style.transform = `translateX(${(index - current) * 100}%)`;
+    });
+  });
+};
+slide();
+
+///////////////////////sticky navbar//////////////////////
+
+const header = document.querySelector(".header");
+const navbarHeight = nav.getBoundingClientRect().height;
+
+const makeSticky = (e) => {
+  const [obj] = e;
+  if (obj.isIntersecting) {
+    nav.classList.remove("sticky");
+  } else {
+    nav.classList.add("sticky");
+  }
+};
+
+const headerObserver = new IntersectionObserver(makeSticky, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navbarHeight}px`,
+});
+
+headerObserver.observe(header);
